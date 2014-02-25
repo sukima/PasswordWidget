333186642ba218b1426f843d7c587a1607e3ed33
Devin Weaver <suki@tritarget.org>
2014-02-24 21:30:34 -0500

    Add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp       = require('gulp');
+var gutil      = require('gulp-util');
+var browserify = require('gulp-browserify');
+var uglify     = require('gulp-uglify');
+var clean      = require('gulp-clean');
+
+var paths = {
+  dest:    'build',
+  scripts: 'lib/PasswordWidget.js'
+};
+
+gulp.task('scripts', function() {
+  gulp.src(paths.scripts)
+    .pipe(browserify())
+    .pipe(gutil.env.production ? uglify() : gutil.noop())
+    .pipe(gulp.dest(paths.dest));
+});
+
+gulp.task('clean', function() {
+  gulp.src(paths.dest, {read: false})
+    .pipe(clean());
+});
+
+gulp.task('default', ['scripts']);
+/* vim:set ts=2 sw=2 et fdm=marker: */
