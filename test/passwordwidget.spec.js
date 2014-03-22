9e1ebb3bdf954576a501d22dd42b69201d33422b
Devin Weaver <suki@tritarget.org>
2014-03-22 12:51:19 -0400

    Handle type checking in constructor

--- test/passwordwidget.spec.js
+++ test/passwordwidget.spec.js
@@ -8,9 +8,34 @@ describe('PasswordWidget', function() {
   });
 
   describe('Constructor', function() {
-    it('takes a selector as a string');
-    it('takes a DOMElement');
-    it('assignes DOMElements to elements property');
+    describe('type checking', function() {
+      var testFn = function(selector) {
+        return function() {
+          return new PasswordWidget(selector);
+        };
+      };
+
+      var typeCheck = function(e) {
+        expect(e).to.be.a(TypeError);
+      };
+
+      it('throws a TypeError when selector is not found', function() {
+        expect(testFn(null)).to.throwException(typeCheck);
+        expect(testFn(undefined)).to.throwException(typeCheck);
+        expect(testFn('')).to.throwException(typeCheck);
+        expect(testFn('#element_does_not_exist')).to.throwException(typeCheck);
+      });
+
+      it('throws a TypeError when DOMElement is not of tye <INPUT>', function() {
+        var dom_element = document.createElement('li');
+        expect(testFn(dom_element)).to.throwException(typeCheck);
+      });
+
+      it('does not throw an error when selector is found and is of type <INPUT>', function() {
+        var dom_element = document.createElement('input');
+        expect(testFn(dom_element)).to.not.throwException();
+      });
+    });
   });
 
   describe('::attach', function() {
