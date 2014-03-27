f0439c1cd8c037850abd736ac6245b6f51692631
Devin Weaver <suki@tritarget.org>
2014-03-26 22:48:49 -0400

    Remove poorly writen specs

--- test/passwordwidget.spec.js
+++ test/passwordwidget.spec.js
@@ -1,41 +1,17 @@
 var PasswordWidget = require('../lib/passwordwidget');
 
 describe('PasswordWidget', function() {
-  var sandbox = sinon.sandbox.create();
+  beforeEach(function() {
+    fixtures.set('<input id="test-input" type="password"/>');
+    this.$pwNode = $('#test-input',  fixtures.window().document);
+    this.pwWidget = new PasswordWidget(this.$pwNode.get(0));
+  });
 
   afterEach(function() {
-    sandbox.restore();
+    fixtures.cleanUp();
   });
 
   describe('Constructor', function() {
-    describe('type checking', function() {
-      var testFn = function(selector) {
-        return function() {
-          return new PasswordWidget(selector);
-        };
-      };
-
-      var typeCheck = function(e) {
-        expect(e).to.be.a(TypeError);
-      };
-
-      it('throws a TypeError when selector is not found', function() {
-        expect(testFn(null)).to.throwException(typeCheck);
-        expect(testFn(undefined)).to.throwException(typeCheck);
-        expect(testFn('')).to.throwException(typeCheck);
-        expect(testFn('#element_does_not_exist')).to.throwException(typeCheck);
-      });
-
-      it('throws a TypeError when DOMElement is not of tye <INPUT>', function() {
-        var dom_element = document.createElement('li');
-        expect(testFn(dom_element)).to.throwException(typeCheck);
-      });
-
-      it('does not throw an error when selector is found and is of type <INPUT>', function() {
-        var dom_element = document.createElement('input');
-        expect(testFn(dom_element)).to.not.throwException();
-      });
-    });
   });
 
   describe('::attach', function() {
