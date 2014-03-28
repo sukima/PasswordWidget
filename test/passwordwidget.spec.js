6ac7535ca1021753a3e7f6ee3d8ae575b411a162
Devin Weaver <suki@tritarget.org>
2014-03-27 20:51:37 -0400

    Fix broken spec in IE and PhantomJS
    Seems both IE8 and PhantomJS need a lot of tender love and care to
    dispatch a keyboard event. Had to open several conditionals to handle
    them all.
    
    This is the only spec that needs this help as once it works we can then
    use the internal event system of PasswordWidget and never have to worry
    about the keyup event again.
    
    For reference IE8 lacks a dispatchEvent allowing a simple if/else to
    detect. Phantom JS however doesn't support the standard event
    constructors (yes, there is a languished [issue reported][1] about it)
    but it does support dispatchEvent causing a difficulty in testing for
    it. I used a try/catch to trap if it fails to create the event per
    standards and fall back to the deprecated createEvent method that is
    supported by PhantomJS.
    
    [1]: https://github.com/ariya/phantomjs/issues/11289

--- test/passwordwidget.spec.js
+++ test/passwordwidget.spec.js
@@ -25,17 +25,6 @@ describe('PasswordWidget', function() {
     });
   });
 
-  describe('Events', function() {
-    it('emits a "update" event when input emits a "keyup" event', function() {
-      var callback = sinon.stub();
-      this.pwWidget.on('update', callback);
-      // Have to use dispatchEvent because jQuery.trigger will not fire an
-      // event attached via addEventListener.
-      this.$pwNode.get(0).dispatchEvent(new Event('keyup'));
-      sinon.assert.called(callback);
-    });
-  });
-
   describe('::attach', function() {
   });
 
@@ -43,6 +32,42 @@ describe('PasswordWidget', function() {
   });
 
   describe('::detach', function() {
+  });
+
+  describe('Events', function() {
+    it('emits a "update" event when input emits a "keyup" event', function() {
+      var evt;
+      var callback = sinon.stub();
+      var node = this.$pwNode.get(0);
+      this.pwWidget.on('update', callback);
+
+      // Have to use dispatchEvent/fireEvent because jQuery.trigger will not
+      // fire an event attached via addEventListener. Each environment has an
+      // unusual way to trigger a keyup event.
+      if (node.dispatchEvent) {
+        // Sane browsers
+        try {
+          // Chrome, Safari, Firefox
+          evt = new KeyboardEvent('keyup');
+        } catch (e) {
+          // PhantomJS (wat!)
+          evt = document.createEvent('KeyboardEvent');
+          evt.initEvent('keyup', true, false);
+        }
+        evt.keyCode = 32;
+        node.dispatchEvent(evt);
+      } else {
+        // IE 8
+        evt = document.createEventObject('KeyboardEvent');
+        evt.keyCode = 32;
+        node.fireEvent('onkeyup', evt);
+      }
+
+      sinon.assert.called(callback);
+    });
+  });
+
+  describe('Unobtrusive DOM manipulations', function() {
   });
 });
 /* vim:set ts=2 sw=2 et fdm=marker: */
