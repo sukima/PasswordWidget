b2887ec726802513913a0d38dcd56f45997cc452
Devin Weaver <suki@tritarget.org>
2014-04-02 15:21:29 -0400

    Add initial example site

---
+++ public/javascript/main.js
@@ -0,0 +1,23 @@
+(function() {
+  var loginWidget, registrationWidget;
+
+  function showInfoPopup(e) {
+    var dialog = $('#password-widget-info-dialog');
+    $('.modal-body', dialog).html(e.content);
+    dialog.modal('show');
+  }
+
+  function attachWidgets() {
+    loginWidget = new PasswordWidget('#login-pass', {
+      showInfo:     false,
+      showGenerate: false
+    })
+    .attach();
+
+    registrationWidget = new PasswordWidget('#reg-pass')
+      .on('showInfo', showInfoPopup)
+      .attach();
+  }
+
+  $(attachWidgets);
+})();
